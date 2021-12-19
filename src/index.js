03f5db95933a3c547005d9b8195d713f34d144b8
Airon Herbert <69558706+aironherbert@users.noreply.github.com>
2021-12-19 14:49:41 -0300

    Mudança de uri

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import {
 } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/api',
+  uri: 'https://misty-voice-2266.fly.dev/api',
   cache: new InMemoryCache()
 });
 
